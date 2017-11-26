5a169129b3c8588f2b698a44b9c3f32073315173
paulina-grunwald <paulina.grunwald@gmail.com>
2017-11-26 17:11:53 +0100

    add decode('utf8').strip()

---
+++ Supporting courses/Programming Foundations with Python/Exercise 6.py
@@ -0,0 +1,26 @@
+#write check profanity programme. The programme will detect rude wrods.
+
+from urllib.request import urlopen
+
+def check_profanity(text_to_check):
+    #connect to the website
+    #connection = urllib.request.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+    url = urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+    output = url.read()
+    output = output.decode('utf8').strip()
+    if "true" in output:
+        print("Profanity Alert !")
+    elif "false" in output:
+        print("The document is secure.")
+    else:
+        print("Can not scan the Document Properly")
+    url.close()
+
+def read_text():
+    quotes = open(r"C:\Personal\GitHub\Udacity-Full-Stack-Web-Developer-Nanodegree\Supporting courses\Programming Foundations with Python\text.txt")
+    contents_of_file = quotes.read()
+    contents_of_file = contents_of_file.replace(" ", "")
+    contents_of_file = contents_of_file.replace('\n', '')
+    quotes.close()
+    check_profanity(contents_of_file)
+read_text()
