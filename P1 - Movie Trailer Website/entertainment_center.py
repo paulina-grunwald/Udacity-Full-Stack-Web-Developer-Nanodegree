6c12e14bb99bb5950a376bc92b07a5d14fc2756c
paulina-grunwald <paulina.grunwald@gmail.com>
2017-11-27 22:39:55 +0100

    Update the code to finish the project

--- P1 - Movie Trailer Website/entertainment_center.py
+++ P1 - Movie Trailer Website/entertainment_center.py
@@ -15,16 +15,29 @@ under_the_tuscan_sun = media.Movie("Under the Tuscan Sun",
                      "https://upload.wikimedia.org/wikipedia/en/8/8e/Under_the_tuscan_sun_poster.jpg",
                      "https://www.youtube.com/watch?v=vdJGMZDY0-8")
 
-#create instance of the Under the Toscan sun movie
-under_the_tuscan_sun = media.Movie("Under the Tuscan Sun",
-                     "Story of a writer impulsively buys a villa in Tuscany in order to change her life",
-                     "https://upload.wikimedia.org/wikipedia/en/8/8e/Under_the_tuscan_sun_poster.jpg",
-                     "https://www.youtube.com/watch?v=vdJGMZDY0-8")
+#create instance of the Hachi: A Dog's Tale movie
+hachi = media.Movie("Hachi: A Dog's Tale",
+                     "A college professor's bond with the abandoned dog he takes into his home.",
+                     "https://images-na.ssl-images-amazon.com/images/M/MV5BYTNhZDFkN2ItMmZjNy00ODUwLTk1Y2MtMDZhYTA2N2MyNDU5XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+                     "https://www.youtube.com/watch?v=mhEHr7B1QiU")
 
 #create instance of Race movie
 race = media.Movie("Race",
                      "Athlete Jesse Owens, who won a record-breaking four gold medals at the 1936 Berlin Olympic Games",
                      "https://upload.wikimedia.org/wikipedia/en/0/02/Race_2016_film_poster.jpg",
                      "https://www.youtube.com/watch?v=E31LnSw47xo")
-print(race.storyline)
-race.show_trailer()
+
+john_wick = media.Movie("John Wick: Chapter 2 Action",
+                     "Retired super-assassin John Wick's plans to resume a quiet civilian life",
+                     "http://aisleseat.com/johnwick2.jpg",
+                     "https://www.youtube.com/watch?v=nMqETeQrgqU")
+
+my_neighbor_totoro = media.Movie("My Neighbor Totoro",
+                     "Japanese animated fantasy film written and directed by Hayao Miyazaki ",
+                     "https://upload.wikimedia.org/wikipedia/en/0/02/My_Neighbor_Totoro_-_Tonari_no_Totoro_%28Movie_Poster%29.jpg",
+                     "https://www.youtube.com/watch?v=92a7Hj0ijLs")
+
+movies = [spirited_away, under_the_tuscan_sun, hachi, race, john_wick, my_neighbor_totoro]
+#print(race.storyline)
+#race.show_trailer()
+fresh_tomatoes.open_movies_page(movies)
--- P1 - Movie Trailer Website/media.py
+++ P1 - Movie Trailer Website/media.py
@@ -3,7 +3,7 @@ import webbrowser
 class Movie():
 
     ###Class for representing a movie
-    def __init__(self, title, storyline, poster_url, trailer_url):
+    def __init__(self, movie_title, movie_storyline, poster_image_url, trailer_youtube_url):
         """ Inits a Movie object
         Args:
         title = a string of the movie's title
@@ -11,10 +11,10 @@ class Movie():
         poster_url = a string containing a URL to a poster image
         trailer_url = a string containing a youtube URL of the movie's trailer
         """
-        self.title = title
-        self.storyline = storyline
-        self.poster_url = poster_url
-        self.trailer_url = trailer_url
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
 
 
     def show_trailer(self):
