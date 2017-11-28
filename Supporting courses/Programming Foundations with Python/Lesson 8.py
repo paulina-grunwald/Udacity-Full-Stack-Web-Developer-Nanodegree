49a24f80003d3654ac63e3c79d3ed292d37dfe35
paulina-grunwald <paulina.grunwald@gmail.com>
2017-11-28 17:20:50 +0100

    Update exercise for lesson 8
    add  arguments and instances on class Parent()

--- Supporting courses/Programming Foundations with Python/Lesson 8.py
+++ Supporting courses/Programming Foundations with Python/Lesson 8.py
@@ -1,2 +1,7 @@
 class Parent():
     def __int__(self, last_name, eye_color):
+        print("Parent Consttructor Called")
+        self.last_name = last_name
+        self.eye_color = eye_color
+
+billy_cyrus = Parent("Cyrus", "blue")
