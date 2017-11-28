6f71e23e10dde421d21bfe580a3f41a8375248d9
paulina-grunwald <paulina.grunwald@gmail.com>
2017-11-28 21:40:25 +0100

    Update code for Lesson 8

--- Supporting courses/Programming Foundations with Python/Lesson 8.py
+++ Supporting courses/Programming Foundations with Python/Lesson 8.py
@@ -1,7 +1,35 @@
 class Parent():
-    def __int__(self, last_name, eye_color):
-        print("Parent Consttructor Called")
+    def __init__(self, last_name, eye_color):
+        print("Parent Constructor Called")
         self.last_name = last_name
         self.eye_color = eye_color
 
-billy_cyrus = Parent("Cyrus", "blue")
+    def show_info(self):
+        print("Last Name - " +self.last_name)
+        print("Eye Color - " +self.eye_color)
+
+#class child inherits form class parent
+#it can inherit or reuse all what is available in class paret
+class Child(Parent):
+    def __init__(self, last_name, eye_color, number_of_toys):
+        print("Child Constructor Called")
+        Parent.__init__(self, last_name, eye_color)
+        self.number_of_toys = number_of_toys
+
+    def show_info(self):
+        print("Last Name - " + self.last_name)
+        print("Eye Color - " + self.eye_color)
+        print("Number of toys - " +str(self.number_of_toys))
+#create instance of class Parent
+#billy_cyrus = Parent("Cyrus", "blue")
+#print billy cyrus's last name
+#print(billy_cyrus.last_name)
+#billy_cyrus.show_info()
+
+#create instance of class child
+miley_cyrus = Child("Cyrus", "green", 5)
+#print(miley_cyrus.number_of_toys)
+#print(miley_cyrus.last_name)
+
+#method overriding
+miley_cyrus.show_info()
