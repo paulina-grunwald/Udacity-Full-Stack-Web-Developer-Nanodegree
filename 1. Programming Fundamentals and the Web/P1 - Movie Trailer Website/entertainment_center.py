be0210054843a31dbff682cd1d92dfdbdc367e32
Paulina Grunwald <paulina.grunwald@gmail.com>
2017-12-06 23:30:18 +0100

    update code
    add # NOQA after the urls

--- 1. Programming Fundamentals and the Web/P1 - Movie Trailer Website/entertainment_center.py
+++ 1. Programming Fundamentals and the Web/P1 - Movie Trailer Website/entertainment_center.py
@@ -11,28 +11,29 @@ import fresh_tomatoes
 spirited_away = media.Movie("Spirited Away",
                             "Travel of ten-year old Chihiro "
                             "and her parents to their new home",
-                            "http://img.moviepostershop.com/spirited-away-movie-poster-2002-1010727261.jpg",
+                            "http://img.moviepostershop.com/spirited-away-movie-poster-2002-1010727261.jpg",  # NOQA
                             "https://www.youtube.com/watch?v=ByXuk9QqQkk")
 
 # create instance of the Under the Toscan sun movie
 under_the_tuscan_sun = media.Movie("Under the Tuscan Sun",
                                    "Story of a writer impulsively buys "
-                                   "a villa in Tuscany in order to change her life",
-                                   "https://upload.wikimedia.org/wikipedia/en/8/8e/Under_the_tuscan_sun_poster.jpg",
-                                   "https://www.youtube.com/watch?v=vdJGMZDY0-8")
+                                   "a villa in Tuscany in order to "
+                                   "change her life",
+                                   "https://upload.wikimedia.org/wikipedia/en/8/8e/Under_the_tuscan_sun_poster.jpg",  # NOQA
+                                   "https://www.youtube.com/watch?v=vdJGMZDY0-8")  # NOQA
 
 # create instance of the Hachi: A Dog's Tale movie
 hachi = media.Movie("Hachi: A Dog's Tale",
                     "A college professor's bond with the "
                     "abandoned dog he takes into his home.",
-                    "https://images-na.ssl-images-amazon.com/images/M/MV5BYTNhZDFkN2ItMmZjNy00ODUwLTk1Y2MtMDZhYTA2N2MyNDU5XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+                    "https://images-na.ssl-images-amazon.com/images/M/MV5BYTNhZDFkN2ItMmZjNy00ODUwLTk1Y2MtMDZhYTA2N2MyNDU5XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",  # NOQA
                     "https://www.youtube.com/watch?v=mhEHr7B1QiU")
 
 # create instance of Race movie
 race = media.Movie("Race",
                    "Athlete Jesse Owens, who won a record-breaking "
                    "four gold medals at the 1936 Berlin Olympic Games",
-                   "https://upload.wikimedia.org/wikipedia/en/0/02/Race_2016_film_poster.jpg",
+                   "https://upload.wikimedia.org/wikipedia/en/0/02/Race_2016_film_poster.jpg",  # NOQA
                    "https://www.youtube.com/watch?v=E31LnSw47xo")
 
 # create instance of John Wick movie
@@ -46,7 +47,7 @@ john_wick = media.Movie("John Wick: Chapter 2 Action",
 my_neighbor_totoro = media.Movie("My Neighbor Totoro",
                                  "Japanese animated fantasy film written "
                                  "and directed by Hayao Miyazaki ",
-                                 "https://upload-assets.vice.com/files/2015/04/21/142962556803___Wrma7B3.gif",
+                                 "https://upload-assets.vice.com/files/2015/04/21/142962556803___Wrma7B3.gif",  # NOQA
                                  "https://www.youtube.com/watch?v=92a7Hj0ijLs")
 
 # create array with all the movies
