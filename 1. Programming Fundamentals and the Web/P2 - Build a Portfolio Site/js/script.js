b8d3607f9039a5e086ec276c997e6528e77e8e8e
Paulina Grunwald <paulina.grunwald@gmail.com>
2017-12-24 19:36:04 +0000

    rename file

--- 1. Programming Fundamentals and the Web/P2 - Build a Portfolio Site/js/script.js
+++ 1. Programming Fundamentals and the Web/P2 - Build a Portfolio Site/js/script.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
+$( document ).ready(function() {
 	$(".nav-link").on("click", function(e){
 		e.preventDefault();
 		var hash = this.hash;
 		console.log(hash);
 		$("html, body").animate({
-			scrollTop: $(hash).offet().top},1000,function(){
+			scrollTop: $(hash).offset().top},1000,function(){
 			window.location.hash=hash;
 
 		})
