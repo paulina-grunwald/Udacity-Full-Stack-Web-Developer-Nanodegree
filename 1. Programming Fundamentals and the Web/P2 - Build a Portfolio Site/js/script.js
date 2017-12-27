f001f61401abb6b6ebba5bf2a8f7505e2b5136f4
Paulina Grunwald <paulina.grunwald@gmail.com>
2017-12-27 16:54:39 +0000

    Update script.js

--- 1. Programming Fundamentals and the Web/P2 - Build a Portfolio Site/js/script.js
+++ 1. Programming Fundamentals and the Web/P2 - Build a Portfolio Site/js/script.js
@@ -1,12 +1,27 @@
-$( document ).ready(function() {
-	$(".nav-link").on("click", function(e){
-		e.preventDefault();
-		var hash = this.hash;
-		console.log(hash);
-		$("html, body").animate({
-			scrollTop: $(hash).offset().top},1000,function(){
-			window.location.hash=hash;
 
-		})
-	});
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+        var scroll;
+
+        // Immediately stop if no anchors are found.
+        if (!this.validAnchor && !$target.length) {
+          return;
+        }
+        
+        if($(window).scrollTop()==0){
+            scroll =  ($target.offset().top) - 160
+        }else{
+            scroll =  ($target.offset().top) - 60
+        }
+        $('html, body').stop().animate({
+            'scrollTop': scroll
+        }, 900, 'swing', function () {
+            //window.location.hash = target;
+        });
+    });
 });
+
