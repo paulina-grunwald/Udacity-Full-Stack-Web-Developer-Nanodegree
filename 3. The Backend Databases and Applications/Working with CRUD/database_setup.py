364ffcb4e817666c47ff6d50d82117fa6fe8dd91
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-01-27 10:11:33 +0100

    Add Classes to database_setup.py
    Add MenuItem and Restaurant classses to the database_setup.py

--- 3. The Backend Databases and Applications/Working with CRUD/database_setup.py
+++ 3. The Backend Databases and Applications/Working with CRUD/database_setup.py
@@ -9,10 +9,29 @@ from sqlalchemy.orm import relationship
 
 from sqlalchemy import create_engine 
 
+# Create instance of the declarative base
 Base = declarative_base()
 
-###
+class Restaurant(Base):
+	__tablename__ = "restaurant"
+	name = Column(String(80), nullable = False)
+	id = Column(Integer, primary_key = True)
 
+
+
+class MenuItem(Base):
+	__tablename__ = "menu_item"
+	name = 
+	id =
+	course =
+	description =
+	restaurant_id = 
+	restaurant = relationship(Restaurant)
+
+
+
+# Create instance of create_engine class
 engine = create_engine('sqlite://restaurantmenu.db')
 
+# Goes to database and adds classes
 Base.metadata.create_all(engine)
