0bb46f47db91bdeceb336dd44403194553d5724f
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-01-28 20:18:48 +0100

    update database_setup.py
    Add mappers to MenuItem class. Correct typo on line 34 and 38.

--- 3. The Backend Databases and Applications/Working with CRUD/database_setup.py
+++ 3. The Backend Databases and Applications/Working with CRUD/database_setup.py
@@ -1,37 +1,37 @@
 # Import modules
+import os
 import sys
-import sqlalchemy import 
-Column, ForeginKey, Integer, String
-
-from sqlalchemy.ext.declarative import declarative database
-
-from sqlalchemy.orm import relationship 
-
-from sqlalchemy import create_engine 
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
 
 # Create instance of the declarative base
 Base = declarative_base()
 
 class Restaurant(Base):
+	# Add table information
 	__tablename__ = "restaurant"
-	name = Column(String(80), nullable = False)
+	# Add mappers
 	id = Column(Integer, primary_key = True)
-
-
+	name = Column(String(250), nullable = False)
 
 class MenuItem(Base):
+	# Add table information
 	__tablename__ = "menu_item"
-	name = 
-	id =
-	course =
-	description =
-	restaurant_id = 
+	# Add mappers
+	name = Column(String(80), nullable = False)
+	id = Column(Integer, primary_key = True)
+	description = Column(String(250))
+	price = Column(String(8))
+	course = Column(String(250))
+	restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
 	restaurant = relationship(Restaurant)
 
 
 
 # Create instance of create_engine class
-engine = create_engine('sqlite://restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenu.db')
 
 # Goes to database and adds classes
 Base.metadata.create_all(engine)
