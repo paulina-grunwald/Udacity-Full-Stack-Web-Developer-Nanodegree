9b6ee7cdcb3fa1553f01c8ed2da5f90b11503697
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-02-11 17:19:23 +0100

    Update webserver.py
    Create the website that lists all the restaurants in the restaurants.db
    database and link to edit and delete restaurant. These links do not work
    yet. Added query to output all the restaurants from the database

--- 3. The Backend Databases and Applications/Course Exercises/Restaurant/webserver.py
+++ 3. The Backend Databases and Applications/Course Exercises/Restaurant/webserver.py
@@ -1,11 +1,11 @@
-# Build web server 
+# Build web server
 
 # Import modules
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 # Import common gate interface library
 import cgi
 
-# import CRUD Operations 
+# import CRUD Operations
 from database_setup import Base, Restaurant, MenuItem
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -22,21 +22,27 @@ session = DBSession()
 class WebServerHandler(BaseHTTPRequestHandler):
     # Handle all GET requests
     def do_GET(self):
-        # Look for URL that ends with restaurant
-        if self.path.endswith("/restaurant"):
-            # Query all restaurants name
-            restaurants = session.query(Restaurant).all()
-            output += "<html><body>"
+        try:
+            # Look for URL that ends with restaurant
+            if self.path.endswith("/restaurants"):
+                # Query all restaurants name
+                restaurants = session.query(Restaurant).all()
+                output = ""
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output += "<html><body>"
                 for restaurant in restaurants:
                     output += restaurant.name
                     output += "</br>"
-                    # Add Edit and Delete Links
+                    # Add Edit link
                     output += "<a href ='#'>Edit</a>"
                     output += "</br>"
+                    # Add Delete link
                     output += "<a href ='#'>Delete</a>"
                     output += "</br>"
-                output += "</body></html>"
-                self.wfile.write(output)
+                    output += "</body></html>"
+                    self.wfile.write(output)
                 return
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
@@ -44,35 +50,16 @@ class WebServerHandler(BaseHTTPRequestHandler):
 
 
 
-    def do_POST(self):
-        try:
-            self.send_response(301)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            ctype, pdict = cgi.parse_header(
-                self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
-            output = ""
-            output += "<html><body>"
-            output += " <h2> Okay, how about this: </h2>"
-            output += "<h1> %s </h1>" % messagecontent[0]
-            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-            output += "</body></html>"
-            self.wfile.write(output)
-            print output
-        except:
-            pass
 
-# Main method 
+
+# Main method
 def main():
     # Add try/except block
     try:
         # Define port
         port = 8080
         # Set host address to empty and specify port
-        # Create webserver 
+        # Create webserver
         server = HTTPServer(('', port), WebServerHandler)
         # Add print statement to see if the server is running
         print "Web Server running on port %s" % port
