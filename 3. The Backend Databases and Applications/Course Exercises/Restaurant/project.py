2dae685adc0e3e168c2e3aa57ea358d25d4c4e68
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-02-11 18:31:00 +0100

    update project.py
    import crud operations and create session to connect to the
    restaurantmenu.db

--- 3. The Backend Databases and Applications/Course Exercises/Restaurant/project.py
+++ 3. The Backend Databases and Applications/Course Exercises/Restaurant/project.py
@@ -5,6 +5,20 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# import CRUD Operations 
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+# Create session and connect to DB
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+
 # add decorators
 @app.route('/')
 @app.route('/hello')
