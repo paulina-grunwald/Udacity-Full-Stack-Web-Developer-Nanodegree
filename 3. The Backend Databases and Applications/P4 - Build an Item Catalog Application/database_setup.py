3a4a7c819c5435dee9f96162853204815840b738
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-02-20 20:20:20 +0100

    create database_setup.py
    database_setup.py is a python script that creates tables for japanese
    food database. It contains category table for various types of food as
    well as table for different dishes. SQLAlchemy is imported to this
    script.

---
+++ 3. The Backend Databases and Applications/P4 - Build an Item Catalog Application/database_setup.py
@@ -0,0 +1,38 @@
+# Import modules
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+# Create instance of the declarative base
+Base = declarative_base()
+
+class Category(Base):
+	# Add table representation
+	__tablename__ = "category"
+	# Add mappers with attributes
+	id = Column(Integer, primary_key = True)
+	name = Column(String(250), nullable = False)
+
+class Dish(Base):
+	# Add table representation
+	__tablename__ = "dish"
+	# Add mappers with attributes
+	name = Column(String(80), nullable = False)
+	id = Column(Integer, primary_key = True)
+	description = Column(String(250))
+	createdDate = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+	image = Column(String)
+	course = Column(String(250))
+	category_id = Column(Integer, ForeignKey('category.id'))
+	category = relationship(Category)
+
+
+
+# Create instance of create_engine class
+engine = create_engine('sqlite:///japanesefood.db')
+
+# Goes to database and adds classes
+Base.metadata.create_all(engine)
