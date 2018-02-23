dc5c3f13ec40a28c8a5d6d39ec571d4f0f829084
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-02-23 18:36:48 +0100

    Create session.py
    This script was used to check if newly created japanesefood.db (created
    using database_create.py, data was inserted using japfood.py) was
    working as expected. All the code was run in git bash.

---
+++ 3. The Backend Databases and Applications/P4 - Build an Item Catalog Application/session.py
@@ -0,0 +1,31 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///japanesefood.db')
+Base.metadata.bind=engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, Dish
+
+engine = create_engine('sqlite:///japanesefood.db')
+Base.metadata.bind=engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+# Below written queries have to be run by copping them into the git bash 
+# Query first Category name 
+first = session.query(Category).first()
+first.name
+# u'Rice Dishes'
+
+# Query all the Dishes in the database 
+item = session.query(Dish).all()
+for item in items:
+	print item.name
+	print item.description
---
+++ 3. The Backend Databases and Applications/P4 - Build an Item Catalog Application/webserver.py
@@ -0,0 +1,51 @@
+ #Build web server
+
+# Import modules
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
+
+# Import common gate interface library
+from database_setup import Base, Category, Dish
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Create session and connect to DB
+engine = create_engine('sqlite:///japanesefood.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+class webServerHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        try:
+            if self.path.endswith("/categories"):
+                categories = session.query(Category).all()
+                output = ""
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output += "<html><body>"
+                for category in categories:
+                    output += category.name
+                    output += "</br></br></br>"
+
+                output += "</body></html>"
+                self.wfile.write(output)
+                return
+        except IOError:
+            self.send_error(404, 'File Not Found: %s' % self.path)
+
+
+def main():
+    try:
+        server = HTTPServer(('', 8080), webServerHandler)
+        print 'Web server running...open localhost:8080/restaurants in your browser'
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print '^C received, shutting down server'
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
