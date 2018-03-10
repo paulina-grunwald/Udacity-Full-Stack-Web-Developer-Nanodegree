711a91340eb49751ffc88254b24909233e9b2d58
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-03-10 12:38:03 +0100

    Update database_setup.py
    - add User table and serialize function in order to return object data
    in easily serializeable format
    - change name of the newly created database

--- 3. The Backend Databases and Applications/P4 - Build an Item Catalog Application/database_setup.py
+++ 3. The Backend Databases and Applications/P4 - Build an Item Catalog Application/database_setup.py
@@ -10,6 +10,14 @@ import datetime
 # Create instance of the declarative base
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
 class Category(Base):
 	# Add table representation
 	__tablename__ = "category"
@@ -17,6 +25,15 @@ class Category(Base):
 	id = Column(Integer, primary_key = True)
 	name = Column(String(250), nullable = False)
 	dishes = relationship("Dish", cascade="all, delete-orphan")
+	user = relationship(User)
+
+	@property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'name'         : self.name,
+           'id'           : self.id,
+       }
 
 class Dish(Base):
 	# Add table representation
@@ -29,11 +46,21 @@ class Dish(Base):
 	image = Column(String)
 	category_id = Column(Integer, ForeignKey('category.id'))
 	category = relationship(Category)
+	user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User) 
 
-
+	@property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'name'         		 : self.name,
+           'description'         : self.description,
+           'id'         		 : self.id,
+           'createdDate'         : self.createdDate,
+       }
 
 # Create instance of create_engine class
-engine = create_engine('sqlite:///japanesefood.db')
+engine = create_engine('sqlite:///japanesefoodwithusers.db')
 
 # Goes to database and adds classes
 Base.metadata.create_all(engine)
