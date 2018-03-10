ecd0eb916333279dfbc1d813c230651b2b41b4be
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-03-10 09:00:59 +0100

    Add new table
    Add new table user which contains: id, name, e-mail and picture. Link
    that table with Restaurant and MenuItem tables using ForeginKey user.id.

--- 3. The Backend Databases and Applications/Course Exercises/OAuth2.0/database_setup.py
+++ 3. The Backend Databases and Applications/Course Exercises/OAuth2.0/database_setup.py
@@ -5,11 +5,20 @@ from sqlalchemy import create_engine
  
 Base = declarative_base()
 
+class User(Base):
+    __username__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
 class Restaurant(Base):
     __tablename__ = 'restaurant'
    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -19,6 +28,9 @@ class Restaurant(Base):
            'id'           : self.id,
        }
  
+
+
+
 class MenuItem(Base):
     __tablename__ = 'menu_item'
 
@@ -30,6 +42,8 @@ class MenuItem(Base):
     course = Column(String(250))
     restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User) 
 
 
     @property
@@ -45,7 +59,7 @@ class MenuItem(Base):
 
 
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
  
 
 Base.metadata.create_all(engine)
