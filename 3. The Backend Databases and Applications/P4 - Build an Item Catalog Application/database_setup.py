0495be84b5c3bd7d87bffacad926a4e5f9542c46
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-03-11 11:21:59 +0100

    update database_setup
    add serialize function, add foregin key to category table, change name
    of the output database

--- 3. The Backend Databases and Applications/P4 - Build an Item Catalog Application/database_setup.py
+++ 3. The Backend Databases and Applications/P4 - Build an Item Catalog Application/database_setup.py
@@ -18,6 +18,13 @@ class User(Base):
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
 
+    @property
+    def serialize(self):
+    	return {
+    		'name' : self.name,
+    		'id' : self.id,
+    	}
+
 class Category(Base):
 	# Add table representation
 	__tablename__ = "category"
@@ -25,15 +32,16 @@ class Category(Base):
 	id = Column(Integer, primary_key = True)
 	name = Column(String(250), nullable = False)
 	dishes = relationship("Dish", cascade="all, delete-orphan")
-	user = relationship(User)
+	user_id = Column(Integer, ForeignKey('user.id'))
 
 	@property
-    def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-       }
+	def serialize(self):
+		return {
+			'name'	:	self.name,
+			'id'	:	self.id,
+			'user_id' : self.user_id
+		}
+
 
 class Dish(Base):
 	# Add table representation
@@ -47,20 +55,21 @@ class Dish(Base):
 	category_id = Column(Integer, ForeignKey('category.id'))
 	category = relationship(Category)
 	user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User) 
+	user = relationship(User)
 
 	@property
-    def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         		 : self.name,
-           'description'         : self.description,
-           'id'         		 : self.id,
-           'createdDate'         : self.createdDate,
-       }
+	def serialize(self):
+		return {
+			'name' : self.name,
+			'id' : self.id,
+			'description' : self.description,
+			'user_id': self.user_id
+		} 
+
+   
 
 # Create instance of create_engine class
-engine = create_engine('sqlite:///japanesefoodwithusers.db')
+engine = create_engine('sqlite:///japanesefood.db')
 
 # Goes to database and adds classes
 Base.metadata.create_all(engine)
