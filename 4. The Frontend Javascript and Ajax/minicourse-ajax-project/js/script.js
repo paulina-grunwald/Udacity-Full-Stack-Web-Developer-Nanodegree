564f9b90922f49d728378833e157ab7548d65661
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-03-17 22:39:53 +0100

    add script.js
    This script is part of the project can display image of location input
    by the user. This script uses jQuery.

--- 4. The Frontend Javascript and Ajax/minicourse-ajax-project/js/script.js
+++ 4. The Frontend Javascript and Ajax/minicourse-ajax-project/js/script.js
@@ -11,11 +11,19 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
+    // assign streetStr to value of the text inputed in the form with id="street"
+    var streetStr = $('#street').val();
+    var cityStr  = $('#city').val();
+    // create full address
+    var address = streetStr + ', ' + cityStr;
 
-    // YOUR CODE GOES HERE!
+    $greeting.text('Do you want to live in ' + address + '?');
 
+    //create URL
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address + '';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '"/>');
     return false;
 };
 
+// when user clicks submit the loadData function will run
 $('#form-container').submit(loadData);
